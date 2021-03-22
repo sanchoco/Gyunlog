9cbd0792fe7d7d07074aaab9249cca72cc2f3f68
sanchoco <jguuun@gmail.com>
2021-03-22 16:10:22 +0900

    router 설정, api 분리, 코드 정리. sanitizeHtml 적용(XSS 공격 방지)

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const Post = require('./schemas/posting')
+
+// XSS 방지
 
 // MongoDB
 const connect = require('./schemas/db');
@@ -17,131 +18,28 @@ app.use(express.static('public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+// api 라우터
+const apiRouter = require("./routers/crud");
+app.use("/api", [apiRouter]);
+
 // 홈
 app.get('/', (req, res) => {
 	res.render('index');
 })
 
+// 수정 페이지
 app.get('/write', (req, res) => {
 	res.render('write');
 })
 
+// 상세 페이지
 app.get('/detail/:Id', async (req, res) => {
 	res.render('detail');
 })
 
+//수정 페이지
 app.get('/update/:id', (req, res) => {
 	res.render('update');
-})
-
-app.get('/api/list', async (req, res) => {
-	const post = await Post.find().sort({ "date": -1 }).select('postId title writer date');
-	await res.json(post)
-})
-
-app.post('/api/write', async (req, res) => {
-	const data = await req.body;
-
-	const title = data["title"];
-	const writer = data["writer"];
-	const password = data["password"];
-	const content = data["content"];
-	if (!(title && writer && password && content)) {
-		res.json({ msg: "fail" })
-	} else {
-		const lasted = await Post.findOne().sort({ "postId": -1 });
-		let index = 1
-		if (lasted) {
-			index = lasted["postId"] + 1
-		}
-		await Post.create({
-			postId: index,
-			title: title,
-			writer: writer,
-			content: content,
-			password: password
-		});
-		res.json({ msg: "success" })
-	}
-});
-
-app.get('/api/detail/:id', async (req, res) => {
-	const id = req.params.id
-	const post = await Post.findOne({ postId: id })
-		.select('postId title writer content date');
-	const data = {
-		postId: id,
-		title: post["title"],
-		writer: post["writer"],
-		content: post["content"],
-		date: post["date"]
-	}
-	res.json(data)
-})
-
-app.get('/api/update/:id', async (req, res) => {
-	const id = req.params.id
-	const post = await Post.findOne({ postId: id })
-		.select('postId title writer content');
-	const data = {
-		postId: id,
-		title: post["title"],
-		writer: post["writer"],
-		content: post["content"],
-	}
-	res.json(data)
-})
-
-app.put('/api/update/:id', async (req, res) => {
-	const id = req.params.id
-
-	const data = await req.body;
-	const title = data["title"];
-	const writer = data["writer"];
-	const password = data["password"];
-	const content = data["content"];
-	const db_post = await Post.findOne({ postId: id })
-		.select('password');
-	if (!(title && writer && password && content)) {
-		res.json({ msg: "empty" })
-	}
-	else if (db_post["password"] != data["password"]) {
-		res.json({ msg: "fail" })
-	}
-	else {
-		await Post.updateOne({ postId: id }, {
-			title: title,
-			writer: writer,
-			content: content,
-			password: password
-		})
-		.then(() => {
-			res.json({ msg: "success" })
-		})
-		.catch(() => {
-			res.json({ msg: "fail" })
-		})
-	}
-})
-
-app.delete('/api/delete/:id', async (req, res) => {
-	const id = await req.params.id
-	const data = await req.body;
-	const password = data["password"];
-	const db_post = await Post.findOne({ postId: id })
-		.select('password');
-	if (db_post["password"] != password) {
-		res.json({ msg: "fail" })
-	}
-	else {
-		await Post.deleteOne({ postId: id })
-			.then(() => {
-				res.json({ msg: "success" })
-			})
-			.catch(() => {
-				res.json({ msg: "fail" })
-			})
-	}
 })
 
 //listen
---
+++ routers/crud.js
@@ -0,0 +1,124 @@
+const express = require("express");
+const router = express.Router();
+const Post = require('../schemas/posting')
+
+// XSS 방지
+var sanitizeHtml = require('sanitize-html');
+
+// 메인 페이지 리스트 불러오기
+router.get('/list', async (req, res) => {
+	const post = await Post.find().sort({ "date": -1 }).select('postId title writer date');
+	await res.json(post)
+})
+
+// 새로운 게시글 작성 처리
+router.post('/write', async (req, res) => {
+	const data = await req.body;
+
+	const title = sanitizeHtml(data["title"]);
+	const writer = sanitizeHtml(data["writer"]);
+	const password = data["password"];
+	const content = sanitizeHtml(data["content"]);
+	if (!(title && writer && password && content)) {
+		res.json({ msg: "fail" })
+	} else {
+		const lasted = await Post.findOne().sort({ "postId": -1 });
+		let index = 1
+		if (lasted) {
+			index = lasted["postId"] + 1
+		}
+		await Post.create({
+			postId: index,
+			title: title,
+			writer: writer,
+			content: content,
+			password: password
+		});
+		res.json({ msg: "success" })
+	}
+});
+
+// 상세 페이지 정보 가져오기
+router.get('/detail/:id', async (req, res) => {
+	const id = req.params.id
+	const post = await Post.findOne({ postId: id })
+		.select('postId title writer content date');
+	const data = {
+		postId: id,
+		title: sanitizeHtml(post["title"]),
+		writer: sanitizeHtml(post["writer"]),
+		content: sanitizeHtml(post["content"]),
+		date: post["date"]
+	}
+	res.json(data)
+})
+
+// 수정을 위한 이전 정보 가져오기
+router.get('/update/:id', async (req, res) => {
+	const id = req.params.id
+	const post = await Post.findOne({ postId: id })
+		.select('postId title writer content');
+	const data = {
+		postId: id,
+		title: sanitizeHtml(post["title"]),
+		writer: sanitizeHtml(post["writer"]),
+		content: sanitizeHtml(post["content"]),
+	}
+	res.json(data)
+})
+
+// 게시글 수정 처리
+router.put('/update/:id', async (req, res) => {
+	const id = req.params.id
+
+	const data = await req.body;
+	const title = sanitizeHtml(data["title"]);
+	const writer = sanitizeHtml(data["writer"]);
+	const password = sanitizeHtml(data["password"]);
+	const content = sanitizeHtml(data["content"]);
+	const db_post = await Post.findOne({ postId: id })
+		.select('password');
+	if (!(title && writer && password && content)) {
+		res.json({ msg: "empty" })
+	}
+	else if (db_post["password"] != data["password"]) {
+		res.json({ msg: "fail" })
+	}
+	else {
+		await Post.updateOne({ postId: id }, {
+			title: title,
+			writer: writer,
+			content: content,
+			password: password
+		})
+			.then(() => {
+				res.json({ msg: "success" })
+			})
+			.catch(() => {
+				res.json({ msg: "fail" })
+			})
+	}
+})
+
+// 게시글 삭제 처리
+router.delete('/delete/:id', async (req, res) => {
+	const id = req.params.id
+	const data = await req.body;
+	const password = data["password"];
+	const db_post = await Post.findOne({ postId: id })
+		.select('password');
+	if (db_post["password"] != password) {
+		res.json({ msg: "fail" })
+	}
+	else {
+		await Post.deleteOne({ postId: id })
+			.then(() => {
+				res.json({ msg: "success" })
+			})
+			.catch(() => {
+				res.json({ msg: "fail" })
+			})
+	}
+})
+
+module.exports = router;
